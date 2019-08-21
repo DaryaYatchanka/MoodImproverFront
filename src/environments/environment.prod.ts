a1bad0a1c1dd048aa772890e3919d67016da7d3e
Darya <Darya.yatchanka@gmail.com>
2019-08-21 14:44:25 -0400

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  url: "https://moodimproverspring.herokuapp.com/",
+  apiUrl: 'https://healthruwords.p.rapidapi.com/v1/quotes/?size=medium&t=Happiness'
 };
